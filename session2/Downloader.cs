f27b7ad0821c503988b0f8837e0353d8ff6ff54e
Gaurav Gogia <16029099+gaurav-gogia@users.noreply.github.com>
2021-02-06 08:45:16 +0530

    removed unnecessary message

--- session2/Downloader.cs
+++ session2/Downloader.cs
@@ -8,7 +8,6 @@ namespace session2
         public string Download(string url)
         {
             WebClient client = new WebClient();
-            Console.WriteLine("Download Complete.");
             return client.DownloadString(url);
         }
     }
